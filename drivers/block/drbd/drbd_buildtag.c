91841a936ed4ae3327e98527b3180c7e8fcdab3d
Michael D Labriola <michael.d.labriola@gmail.com>
2020-07-10 10:25:56 -0400

    drbd: imported sources from upstream 9.0.24-1
    This commit replaces the intree drbd8 sources with upstream drbd9 sources.
    See NOTES file for details

---
+++ drivers/block/drbd/drbd_buildtag.c
@@ -0,0 +1,11 @@
+/* automatically generated. DO NOT EDIT. */
+/* FIXME: not really, i wrote this by hand... ;-)
+ */
+
+#include <linux/drbd.h>
+#include <linux/drbd_config.h>
+
+const char *drbd_buildtag(void)
+{
+	return "GIT-hash: 6e91f89e4016d01512db171d98b3424204d254dc";
+}
