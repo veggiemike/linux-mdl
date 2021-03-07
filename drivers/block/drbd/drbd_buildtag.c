f917ef0547501c3c7b96e5b3483d66e00fbea1a2
Michael D Labriola <michael.d.labriola@gmail.com>
2021-03-07 17:47:28 -0500

    drbd: imported sources from upstream 9.1.0
    Technically, this was done after I added a couple commits to 9.1.0 to get
    it compiling w/ 5.11 kernels.
    
    cd /scrap/drbd
    make KDIR=/scrap/linux-mdl-5.11
    cd /scrap/linux-mdl-5.11/drivers/block/drbd
    ./import-upstream /scrap/drbd

---
+++ drivers/block/drbd/drbd_buildtag.c
@@ -0,0 +1,11 @@
+/* automatically generated. DO NOT EDIT. */
+/* FIXME: not really, i wrote this by hand... ;-)
+ */
+
+#include <linux/drbd.h>
+#include <linux/drbd_config.h>
+
+const char *drbd_buildtag(void)
+{
+	return "GIT-hash: 0e6e6f434e9c0cdf641a08f3bdfc94bbc6bf6668";
+}
