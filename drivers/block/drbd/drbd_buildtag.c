87130a12cbf8c9a84cdd247ea5b95235224b249b
Michael D Labriola <michael.d.labriola@gmail.com>
2021-09-05 19:01:23 -0400

    drbd: imported sources from upstream 9.1.3-mdl
    cd /scrap/drbd
    make KDIR=/scrap/linux-mdl-5.14
    cd /scrap/linux-mdl-5.14/drivers/block/drbd
    ./import-upstream /scrap/drbd

---
+++ drivers/block/drbd/drbd_buildtag.c
@@ -0,0 +1,11 @@
+/* automatically generated. DO NOT EDIT. */
+/* FIXME: not really, i wrote this by hand... ;-)
+ */
+
+#include <linux/drbd.h>
+#include <linux/drbd_config.h>
+
+const char *drbd_buildtag(void)
+{
+	return "GIT-hash: dea4f23c993160066049058a63c0fbe6f38952a2";
+}
